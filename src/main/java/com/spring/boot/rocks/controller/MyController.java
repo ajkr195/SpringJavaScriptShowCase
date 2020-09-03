b210e54ca236ff8ecdf7c4a78ba419625072b7c6
ajay <ajkr195@gmail.com>
2020-09-03 18:44:31 -0400

    update

--- src/main/java/com/spring/boot/rocks/controller/MyController.java
+++ src/main/java/com/spring/boot/rocks/controller/MyController.java
@@ -1,16 +1,24 @@
 package com.spring.boot.rocks.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/")
 public class MyController {
 
-	@GetMapping
+	@GetMapping("/")
 	public static String homePage(Model model) {
 
 		return "index";
+
+	}
+	@GetMapping("/modal")
+	public static String modalPage(Model model) {
+
+		return "modal";
 
 	}
 
