06d1addf6df370851d623e2e4031f7e03dbb5d98
ajay <ajkr195@gmail.com>
2020-09-09 15:27:58 -0400

    update

--- src/main/resources/static/js/jsontopresentableview.js
+++ src/main/resources/static/js/jsontopresentableview.js
@@ -1,10 +1,30 @@
 async function getUsers() {
     let url = '/api/userlist';
     try {
-        let res = await fetch(url);
-        return await res.json();
+        let response = await fetch(url);
+        
+        //if (response.status !== 200) {
+        //	alert('Looks like there is a problem. Status Code: ' + response.status );
+        //	return;
+      	//}
+      	//let data = await response.text();
+      	//alert( 'data :: ' + data  );
+      	
+      	//if (response.status === 200) {
+      	// 	alert('response.headers.get(\'Content-Type\') :: ' + response.headers.get('Content-Type'));
+    	//	alert('response.headers.get(\'Date\') :: ' + response.headers.get('Date'));
+    	//	alert('response.status :: ' + response.status);
+    	//	alert('response.statusText :: ' + response.statusText);
+    	//	alert('response.type :: ' + response.type);
+    	//	alert('response.url :: ' + response.url);
+      	//}
+      	
+        
+        return await response.json();
+        
     } catch (error) {
         console.log(error);
+        alert(error);
     }
 }
 
@@ -25,4 +45,23 @@ async function renderUsers() {
 }
 
 renderUsers();
+
+
+
+async function displayUsers() {
+    let users = await getUsers();
+    
+    var htmltabletdtrdata = "";
+					
+	users.forEach((theuser)=>{
+		htmltabletdtrdata += "<tr>";
+		htmltabletdtrdata += "<td>" + theuser.id+"</td>";
+		htmltabletdtrdata += "<td>" + theuser.username+"</td>";
+		htmltabletdtrdata += "<td>" + theuser.useremail+"</td>";
+		htmltabletdtrdata += "<td>" + theuser.useraddress+"</td></tr>";
+	})
+	
+	document.getElementById("userDataTable").innerHTML = htmltabletdtrdata;
+    
+}
 
